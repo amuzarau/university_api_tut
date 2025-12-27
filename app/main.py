46064049f1fefcc35ebeb2dbaa412b3eeb77bca7
amuzarau <andrei.muzarau@gmail.com>
2025-12-28 00:20:54 +0300

    Working inference endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
+from http.client import HTTPException
 from typing import Union
 
 from fastapi import FastAPI
+
+from app.models.student import StudentCreate, Student
+
+from app.db import get_db_connection
 
 app = FastAPI()
 
@@ -13,3 +18,25 @@ def get_root():
 @app.get("/status")
 async def status():
     return {"message": "OK"}
+
+
+@app.post("/student/")
+def create_student(student: StudentCreate):
+    conn, cursor = get_db_connection()
+    try:
+        cursor.execute(
+            "INSERT INTO Students (first_name, last_name) VALUES (%s, %s) RETURNING *",
+            (student.first_name, student.last_name),
+        )
+        new_student = cursor.fetchone()
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        conn.close()
+        raise HTTPException(status_code=500, detail=str(e))
+
+    conn.close()
+    if new_student:
+        return Student(**new_student)
+
+    raise HTTPException(status_code=400, detail="Error creating student")
